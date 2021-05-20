c5eed12f3d33606edc43392d9209bc9957c817cb
Haru0101 <haruki.for.01@gmail.com>
2021-05-20 21:55:55 +0900

    SEOのmetaにはdescriptionではなくtitleを一時的に使用する
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -23,12 +23,12 @@ const Layout = ({
       <Helmet>
         <html lang="ja" />
         <title>{title}</title>
-        <meta name="description" content={description} />
+        <meta name="description" content={title} />
         <meta property="og:site_name" content={title} />
         <meta property="og:image" content={metaImageUrl} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
+        <meta name="twitter:description" content={title} />
         <meta name="twitter:image" content={metaImageUrl} />
       </Helmet>
       {children}
