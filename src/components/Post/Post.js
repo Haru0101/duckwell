da814bbff7d9c1f56f0ed3702fc81451ea664402
Haru0101 <haruki.for.01@gmail.com>
2021-06-07 21:56:16 +0900

    タグだけ削除
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,10 @@
 // @flow strict
 import React from 'react';
 import { Link } from 'gatsby';
+import Author from './Author';
+import Comments from './Comments';
 import Content from './Content';
+import Meta from './Meta';
 import styles from './Post.module.scss';
 import type { Node } from '../../types';
 
@@ -11,7 +14,8 @@ type Props = {
 
 const Post = ({ post }: Props) => {
   const { html } = post;
-  const { title } = post.frontmatter;
+  const { tagSlugs, slug } = post.fields;
+  const { tags, title, date } = post.frontmatter;
 
   return (
     <div className={styles['post']}>
@@ -19,6 +23,15 @@ const Post = ({ post }: Props) => {
 
       <div className={styles['post__content']}>
         <Content body={html} title={title} />
+      </div>
+
+      <div className={styles['post__footer']}>
+        <Meta date={date} />
+        <Author />
+      </div>
+
+      <div className={styles['post__comments']}>
+        <Comments postSlug={slug} postTitle={post.frontmatter.title} />
       </div>
     </div>
   );
